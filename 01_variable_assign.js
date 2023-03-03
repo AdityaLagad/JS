f99529adfb0cae70f661814bb855d037b2b1300e
AdityaLagad <aditya02451@gmail.com>
2023-03-03 22:12:32 +0530

    01_variable_assign
    01_variable_assign created

---
+++ 01_variable_assign.js
@@ -0,0 +1,19 @@
+var fullName; //variable initiatization
+fullName = "Aditya Lagad" //veri  decl
+console.log("Full Name:",fullName);
+fullName = "Aditya D Lagad"
+console.log("Full Name:",fullName);
+
+console.log("*************************************");
+var myLove;
+myLove = "i love only JS"
+console.log("myLove:",myLove);
+console.log("*************************************");
+var firstName = "aditya"
+var lastName = "lagad,"
+var age = "23,"
+var adress = "Parijat Colony Selu,"
+var pinCode = "431503,"
+var gender = "Male,"
+var isMarried = "no"
+console.log(firstName,lastName,age,adress,pinCode,gender,isMarried);
