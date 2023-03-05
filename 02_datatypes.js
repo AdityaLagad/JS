c805631a3953b23baaf7b9b6e382f905c4f4da9b
AdityaLagad <aditya02451@gmail.com>
2023-03-05 15:55:01 +0530

    02_datatypes assig
    02_datatypes assig

---
+++ 02_datatypes.js
@@ -0,0 +1,15 @@
+var num;
+num =02451;
+console.log("num:",num,"type:",typeof num);
+var alpha;
+alpha ="set of Char type";
+console.log("alpha:",alpha,  "type:", typeof alpha);
+var bool;
+bool = true;
+console.log("bool:",bool, "type:", typeof bool);
+
+//que 2
+var example;
+console.log("datatype:",typeof example);
+var example=null;
+console.log("datatype of null:",typeof example);
